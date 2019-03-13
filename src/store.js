fbe34a322258c76610bbe73d86ab02aa663dbf89
Mike Green <mike.green@simplethread.com>
2019-03-13 10:27:36 -0400

    Add redux-thunk middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const middleware = applyMiddleware(thunk, devtools);
 
-export default createStore(rootReducer, devtools);
+export default createStore(rootReducer, middleware);
