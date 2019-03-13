a6aecd4949be3d953e533053192744607e83a8b8
Mike Green <mike.green@simplethread.com>
2019-03-13 11:49:59 -0400

    Use compose to combine middleware functions

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const middleware = applyMiddleware(thunk, devtools);
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f;
+const middleware = compose(
+  applyMiddleware(thunk),
+  devtools
+);
 
 export default createStore(rootReducer, middleware);
